bb66b4cf6f078e362cc263e3406b078cd7f88039
Carl782051063 <67107723+Carl782051063@users.noreply.github.com>
2023-11-17 15:57:05 -0500

    1

--- FFT/digitrec_test.cpp
+++ FFT/digitrec_test.cpp
@@ -4,11 +4,12 @@
 // @brief: testbench for k-nearest-neighbor digit recongnition application
 
 #include <stdio.h>
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <string>
 
-#include "digitrec.h"
+#include "fft.h"
 
 // using namespace std;
 
@@ -18,59 +19,41 @@ int main() {
   outfile.open("out.dat");
 
   // Read input file for the testing set
-  std::string line;
+ 
   std::ifstream myfile("data/testing_set.dat");
+  std::vector<DTYPE> real_array;
+  DTYPE value;
 
   if (myfile.is_open()) {
-    int error = 0;
-    int num_test_insts = 0;
 
-    while (std::getline(myfile, line)) {
-      // Read handwritten digit input and expected digit
-      digit input_digit =
-          strtoul(line.substr(0, line.find(",")).c_str(), NULL, 16);
-      int input_value = strtoul(
-          line.substr(line.find(",") + 1, line.length()).c_str(), NULL, 10);
-
-      // Call design under test (DUT)
-      bit4 interpreted_digit = digitrec(input_digit);
-
-      // Print result messages to console
-      num_test_insts++;
-      std::cout << "#" << std::dec << num_test_insts;
-      std::cout << ": \tTestInstance=" << std::hex << input_digit;
-      std::cout << " \tInterpreted=" << interpreted_digit
-                << " \tExpected=" << input_value;
-      // Print result messages to file
-      outfile << "#" << std::dec << num_test_insts;
-      outfile << ": \tTestInstance=" << std::hex << input_digit;
-      outfile << " \tInterpreted=" << interpreted_digit
-              << " \tExpected=" << input_value;
-
-      // Check for any difference between k-NN interpreted digit vs. expected
-      // digit
-      if (interpreted_digit != input_value) {
-        error++;
-        std::cout << " \t[Mismatch!]";
-        outfile << " \t[Mismatch!]";
+      while (myfile >> value) {
+          real_array.push_back(value);
       }
 
-      std::cout << std::endl;
-      outfile << std::endl;
-    }
+      myfile.close();
 
-    // Report overall error out of all testing instances
-    std::cout << "Overall Error Rate = " << std::setprecision(3)
-              << ((double)error / num_test_insts) * 100 << "%" << std::endl;
-    outfile << "Overall Error Rate = " << std::setprecision(3)
-            << ((double)error / num_test_insts) * 100 << "%" << std::endl;
+      
+  } else{
+    std::cout << "Unable to open file for the testing set 0 !" << std::endl;
+  }
 
-    // Close input file for the testing set
-    myfile.close();
+  std::ifstream myfile_2("data/testing_set_0.dat");
+  std::vector<DTYPE> image_array;
+  DTYPE value_i;
 
-  } else
-    std::cout << "Unable to open file for the testing set!" << std::endl;
+  if (myfile_2.is_open()) {
 
+      while (myfile_2 >> value_i) {
+          image_array.push_back(value_i);
+      }
+
+      myfile_2.close();
+
+  } else{
+    std::cout << "Unable to open file for the testing set 1!" << std::endl;
+  }
+  // Call design under test (DUT)
+      fft(real_array.data(), image_array.data());
   // Close output file
   outfile.close();
 
