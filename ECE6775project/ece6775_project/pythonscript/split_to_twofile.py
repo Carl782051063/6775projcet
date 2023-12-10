093d90c3078e88d4b880c51e4e3a0a100947bcf0
Carl782051063 <67107723+Carl782051063@users.noreply.github.com>
2023-12-10 11:49:18 -0500

    pythoncode

--- ECE6775project/ece6775_project/pythonscript/split_to_twofile.py
+++ ECE6775project/ece6775_project/pythonscript/split_to_twofile.py
@@ -1,21 +1,21 @@
-# 定义两个文件名
+# Define the two filenames
 output_file_col1 = 'column1.txt'
 output_file_col2 = 'column2.txt'
 
-# 打开输入文件和输出文件
+# Open the input file and output files
 with open('data.txt', 'r') as input_file, \
      open(output_file_col1, 'w') as output_file1, \
      open(output_file_col2, 'w') as output_file2:
 
-    # 逐行读取输入文件
+    # Read the input file line by line
     for line in input_file:
-        # 以空格分割每一行
+        # Split each line by space
         columns = line.split()
         if len(columns) >= 2:
-            # 提取第一列和第二列数据
+            # Extract data from the first and second columns
             col1 = columns[0]
             col2 = columns[1]
 
-            # 将数据写入相应的输出文件
+            # Write the data to the respective output files
             output_file1.write(col1 + '\n')
             output_file2.write(col2 + '\n')
